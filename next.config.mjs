03ee05b4e6d2ae2f409a472442e84e213e6a9b19
Murod Akhmedov <murod.london@gmail.com>
2024-11-20 15:21:36 +0000

    Update next config file

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,15 @@ const nextConfig = {
 
     return config;
   },
+  env: {
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    OPENAI_ORG_ID: process.env.OPENAI_ORG_ID,
+    OPENAI_PROJECT_ID: process.env.OPENAI_PROJECT_ID,
+  },
 };
 
 export default nextConfig;
