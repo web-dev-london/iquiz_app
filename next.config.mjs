b2b5d2ccebbc56799bab3be7855b6c20c5bdefc5
Murod Akhmedov <murod.london@gmail.com>
2024-11-20 15:30:01 +0000

    Save unstaged changes

--- next.config.mjs
+++ next.config.mjs
@@ -20,11 +20,6 @@ const nextConfig = {
   env: {
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-    OPENAI_ORG_ID: process.env.OPENAI_ORG_ID,
-    OPENAI_PROJECT_ID: process.env.OPENAI_PROJECT_ID,
   },
 };
 
